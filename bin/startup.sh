e6652a17e60e4adfae21591ad971076c3006ff65
bcarr610 <carrbrandon4@gmail.com>
2024-04-27 18:52:17 -0400

    Add pakcage scripts and test executables

--- bin/startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-sudo source /etc/profile.d/a_vars.sh
-
-echo "Starting ARIA Thermostat..."
-sleep 5
-cd "$ARIA_THERMOSTAT_ROOT"
-epoch=$(date +%s)
-npm start >> "$ARIA_LOGS/$epoch.log" 2>&1
